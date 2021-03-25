1ddb9a97758d2d100f67b4b398a63b7ecbb75a39
chadward <chad.murph.johnson@gmail.com>
2021-03-25 01:51:03 -0700

    canvas clears when appropriate; permissions for who can draw added; minor css/html additions for testing purposes

--- public/chat.js
+++ public/chat.js
@@ -5,6 +5,85 @@
     
     var usersList = document.getElementById('usersList')
 
+        //Whiteboard (drawing board)
+        var canvas = document.getElementsByClassName('whiteboard')[0];  
+        canvas.width = 800;
+        canvas.height = 800;
+    
+        var colors = document.getElementsByClassName('color');
+        var context = canvas.getContext('2d');
+    
+        var current = {
+            color: 'black'
+        };
+        var drawing = false;
+    
+        for (var i = 0; i < colors.length; i++){
+            colors[i].addEventListener('click', onColorUpdate, false);
+        }
+    
+        socket.on('drawing', onDrawingEvent);
+    
+        function drawLine(x0, y0, x1, y1, color, emit){
+            context.beginPath();
+            context.moveTo(x0, y0);
+            context.lineTo(x1, y1);
+            context.strokeStyle = color;
+            context.lineWidth = 4;
+            context.stroke();
+            context.closePath();
+    
+            if (!emit) { return; }
+    
+            socket.emit('drawing', {
+            x0: x0,
+            y0: y0,
+            x1: x1,
+            y1: y1,
+            color: color
+            });
+        }
+    
+        function onMouseDown(e){
+            drawing = true;
+            current.x = e.pageX||e.touches[0].pageX;
+            current.y = e.pageY||e.touches[0].pageY;
+        }
+    
+        function onMouseUp(e){
+            if (!drawing) { return; }
+            drawing = false;
+            drawLine(current.x, current.y, e.pageX||e.touches[0].pageX, e.pageY||e.touches[0].pageY, current.color, true);
+        }
+    
+        function onMouseMove(e){
+            if (!drawing) { return; }
+            drawLine(current.x, current.y, e.pageX||e.touches[0].pageX, e.pageY||e.touches[0].pageY, current.color, true);
+            current.x = e.pageX||e.touches[0].pageX;
+            current.y = e.pageY||e.touches[0].pageY;
+        }
+    
+        function onColorUpdate(e){
+            current.color = e.target.className.split(' ')[1];
+        }
+    
+                // limit the number of events per second
+        function throttle(callback, delay) {
+            var previousCall = new Date().getTime();
+            return function() {
+            var time = new Date().getTime();
+    
+            if ((time - previousCall) >= delay) {
+                previousCall = time;
+                callback.apply(null, arguments);
+            }
+            };
+        }
+    
+        function onDrawingEvent(data){
+            drawLine(data.x0, data.y0, data.x1, data.y1, data.color);
+        }
+
     //User joining room
         // Get username and room from URL
         const {username} = Qs.parse(location.search, {
@@ -33,21 +112,49 @@
     socket.on('drawer', (words) => {
         var word = document.getElementById('word');
         var timer = document.getElementById('timer');
+        var input = document.getElementById('input');
+        input.style.display = 'none';
         timer.style.display = 'flex';
         word.style.display = 'flex';
         word.innerText = '';
         word.innerText = words;
+
+        canvas.addEventListener('mousedown', onMouseDown, false);
+        canvas.addEventListener('mouseup', onMouseUp, false);
+        canvas.addEventListener('mouseout', onMouseUp, false);
+        canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+        
+                //Touch support for mobile devices
+        canvas.addEventListener('touchstart', onMouseDown, false);
+        canvas.addEventListener('touchend', onMouseUp, false);
+        canvas.addEventListener('touchcancel', onMouseUp, false);
+        canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+        
     });
 
     socket.on('set permissions', function(){
         socket.emit('permission');
+        context.clearRect(0, 0, canvas.width, canvas.height);
     })
 
     //remove drawer
     socket.on('not drawer', (words) => {
+        canvas.removeEventListener('mousedown', onMouseDown, false);
+        canvas.removeEventListener('mouseup', onMouseUp, false);
+        canvas.removeEventListener('mouseout', onMouseUp, false);
+        canvas.removeEventListener('mousemove', throttle(onMouseMove, 10), false);
+        
+                //Touch support for mobile devices
+        canvas.removeEventListener('touchstart', onMouseDown, false);
+        canvas.removeEventListener('touchend', onMouseUp, false);
+        canvas.removeEventListener('touchcancel', onMouseUp, false);
+        canvas.removeEventListener('touchmove', throttle(onMouseMove, 10), false);
+
         var word = document.getElementById('word');
         var timer = document.getElementById('timer');
         var span = document.createElement('span');
+        var input = document.getElementById('input');
+        input.style.display = 'flex';
         timer.style.display = 'none';
         word.innerText = '';
         for(let i = 0; i < words.length; i++){
@@ -222,94 +329,4 @@
             }
         }
     })
-
-    //Whiteboard (drawing board)
-    var canvas = document.getElementsByClassName('whiteboard')[0];  
-    canvas.width = 800;
-    canvas.height = 800;
-
-    var colors = document.getElementsByClassName('color');
-    var context = canvas.getContext('2d');
-
-    var current = {
-        color: 'black'
-    };
-    var drawing = false;
-
-    canvas.addEventListener('mousedown', onMouseDown, false);
-    canvas.addEventListener('mouseup', onMouseUp, false);
-    canvas.addEventListener('mouseout', onMouseUp, false);
-    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-    
-            //Touch support for mobile devices
-    canvas.addEventListener('touchstart', onMouseDown, false);
-    canvas.addEventListener('touchend', onMouseUp, false);
-    canvas.addEventListener('touchcancel', onMouseUp, false);
-    canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
-
-    for (var i = 0; i < colors.length; i++){
-        colors[i].addEventListener('click', onColorUpdate, false);
-    }
-
-    socket.on('drawing', onDrawingEvent);
-
-    function drawLine(x0, y0, x1, y1, color, emit){
-        context.beginPath();
-        context.moveTo(x0, y0);
-        context.lineTo(x1, y1);
-        context.strokeStyle = color;
-        context.lineWidth = 4;
-        context.stroke();
-        context.closePath();
-
-        if (!emit) { return; }
-
-        socket.emit('drawing', {
-        x0: x0,
-        y0: y0,
-        x1: x1,
-        y1: y1,
-        color: color
-        });
-    }
-
-    function onMouseDown(e){
-        drawing = true;
-        current.x = e.pageX||e.touches[0].pageX;
-        current.y = e.pageY||e.touches[0].pageY;
-    }
-
-    function onMouseUp(e){
-        if (!drawing) { return; }
-        drawing = false;
-        drawLine(current.x, current.y, e.pageX||e.touches[0].pageX, e.pageY||e.touches[0].pageY, current.color, true);
-    }
-
-    function onMouseMove(e){
-        if (!drawing) { return; }
-        drawLine(current.x, current.y, e.pageX||e.touches[0].pageX, e.pageY||e.touches[0].pageY, current.color, true);
-        current.x = e.pageX||e.touches[0].pageX;
-        current.y = e.pageY||e.touches[0].pageY;
-    }
-
-    function onColorUpdate(e){
-        current.color = e.target.className.split(' ')[1];
-    }
-
-            // limit the number of events per second
-    function throttle(callback, delay) {
-        var previousCall = new Date().getTime();
-        return function() {
-        var time = new Date().getTime();
-
-        if ((time - previousCall) >= delay) {
-            previousCall = time;
-            callback.apply(null, arguments);
-        }
-        };
-    }
-
-    function onDrawingEvent(data){
-        drawLine(data.x0, data.y0, data.x1, data.y1, data.color);
-    }
 })();
